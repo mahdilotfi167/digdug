9832e77bb5feaeae6e92eb96799e193e188d866c
mahdi lotfi <74452648+mahdilotfi167@users.noreply.github.com>
2021-06-29 01:34:36 +0430

    add new propety for game object constructors for saving own map in each game object

--- src/main/java/ir/ac/kntu/map/Map.java
+++ src/main/java/ir/ac/kntu/map/Map.java
@@ -4,47 +4,89 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 import ir.ac.kntu.models.GameObject;
+import ir.ac.kntu.models.Player;
 import ir.ac.kntu.models.Wall;
+import ir.ac.kntu.rigidbody.Position;
 import ir.ac.kntu.util.GameObjectConstructor;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
 import static ir.ac.kntu.Constants.*;
+/**
+ * a tool for rendering game maps
+ */
 public class Map extends Pane implements Serializable {
-    private int[][] grid = {
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1},
-        };
+    private int[][] grid;
     private GameObject[][] pointers;
-    private HashMap<Integer,GameObjectConstructor> constructores;
-    public Map() {
-        pointers = new GameObject[grid.length][grid[0].length];
-        constructores = new HashMap<>();
-        constructores.put(1, Wall::new);
+    private HashMap<Integer,GameObjectConstructor> constructors;
+    private HashMap<Integer,Color> fillers;
+    private int blockScale;
+    GraphicsContext graphicsContext;
+    public Map(int[][] gridCodes,HashMap<Integer,GameObjectConstructor> constructors,HashMap<Integer,Color> fillers,int blockScale,Color background) {
+        this.blockScale = blockScale;
+        this.grid = gridCodes.clone();
+        this.pointers = new GameObject[grid.length][grid[0].length];
+        this.constructors = constructors;
+        this.fillers = fillers;
+        Canvas canvas = new Canvas(grid[0].length * blockScale, grid.length*blockScale);
+        getChildren().add(canvas);
+        graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.setFill(background);
+        graphicsContext.fillRect(0, 0, grid[0].length*blockScale, grid.length*blockScale);
         render();
     }
     private void render() {
-        GameObject current;
+        GameObjectConstructor goc;
+        GameObject gameoObject;
+        Color color;
         for (int i = 0;i<grid.length;i++) {
-            for (int j = 0;j<grid.length;j++) {
-                current = constructores.get(grid[i][j]).getObject(j, i);
-                pointers[i][j] = current;
-                getChildren().add(current);
+            for (int j = 0;j<grid[0].length;j++) {
+                if ((color = fillers.get(grid[i][j])) != null) {
+                    graphicsContext.setFill(color);
+                    graphicsContext.fillRect(blockScale*j,blockScale*i,blockScale,blockScale);
+                }
+                if ((goc = constructors.get(grid[i][j]))!= null) {
+                    gameoObject = goc.getObject(this, j, i);
+                    pointers[i][j] = gameoObject;
+                    getChildren().add(gameoObject);
+                }
             }
         }
     }
-    public static int gridToLayout(int pos) {
-        return pos*BLOCK_SCALE;
+    public GraphicsContext getGraphicsContext() {
+        return graphicsContext;
     }
-    public static int layoutToGrid(int pos) {
-        return pos/BLOCK_SCALE;
+    public void updateObjectPos(GameObject gameObject , int oldX, int oldY) {
+        graphicsContext.setFill(Color.BLACK);
+        graphicsContext.fillRoundRect(gameObject.getCurrentLayoutX(), gameObject.getCurrentLayoutY(), gameObject.getWidth() , gameObject.getHeight(), 5, 5);
+        grid[oldY][oldX] = 0;
+        pointers[oldY][oldX] = null;
+        GameObject pastObject = pointers[gameObject.getGridY()][gameObject.getGridX()];
+        getChildren().remove(pastObject);
+        grid[gameObject.getGridY()][gameObject.getGridX()] = gameObject.getGridCode();
+        pointers[gameObject.getGridY()][gameObject.getGridX()] = gameObject;
+    }
+    public void removeObject(int x,int y) {
+        
+    }
+    public GameObject getObject(int x,int y) {
+        return pointers[y][x];
+    }
+    public double gridToLayout(int pos) {
+        return pos*blockScale;
+    }
+    public int layoutToGrid(double pos) {
+        return (int)(pos/blockScale);
+    }
+    public boolean contains(Position position) {
+        return position.getX() >= 0
+            && position.getY() >= 0
+            && position.getX() < grid[0].length
+            && position.getY() < grid.length;
+    }
+    public static Map load(String path) {
+        return null;
     }
 }
--- src/main/java/ir/ac/kntu/util/GameObjectConstructor.java
+++ src/main/java/ir/ac/kntu/util/GameObjectConstructor.java
@@ -1,8 +1,9 @@
 package ir.ac.kntu.util;
 
+import ir.ac.kntu.map.Map;
 import ir.ac.kntu.models.GameObject;
 
 @FunctionalInterface
 public interface GameObjectConstructor {
-    public GameObject getObject(int x, int y);
+    public GameObject getObject(Map map,int x, int y);
 }
