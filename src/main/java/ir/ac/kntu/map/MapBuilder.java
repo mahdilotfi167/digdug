3390d352a3b0e6034ef19642384c292aa1b7fb09
mahdi lotfi <74452648+mahdilotfi167@users.noreply.github.com>
2021-07-02 02:46:53 +0430

    create map builder model a wrapper class named Draggable.java (base on Decorator pattern) wrapps gameobject and create from those draggable objects in maps draggable decorator used in map builder for creating maps using drag and drop

---
+++ src/main/java/ir/ac/kntu/map/MapBuilder.java
@@ -0,0 +1,120 @@
+package ir.ac.kntu.map;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import static ir.ac.kntu.Constants.*;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import javax.imageio.ImageIO;
+import ir.ac.kntu.data.MapSerializer;
+import ir.ac.kntu.models.Draggable;
+import ir.ac.kntu.models.GameObject;
+import ir.ac.kntu.util.GameObjectConstructor;
+
+public class MapBuilder extends Pane {
+    private Map map;
+    private Stage stage;
+    private Scene scene;
+    private Pane pane;
+    private int currentFiller = 0;
+
+
+    public MapBuilder() {
+        this.stage = new Stage();
+        this.pane = new Pane();
+        this.scene = new Scene(pane,900,627);
+        this.stage.setScene(scene);
+        this.map = new Map(new int[20][25],CONSTRUCTORS , FILLERS, BLOCK_SCALE, Color.BLACK);
+        this.pane.getChildren().add(map);
+        drawPane();
+        stage.show();
+        this.pane.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent e) {
+                if (map.isBlock(map.layoutToGrid(e.getX()), map.layoutToGrid(e.getY()))) {
+                    map.clearBlock(map.layoutToGrid(e.getX()), map.layoutToGrid(e.getY()));
+                } else if (currentFiller != 0) {
+                    map.fill(currentFiller ,map.layoutToGrid(e.getX()), map.layoutToGrid(e.getY()));
+                }
+            }
+        });
+        this.stage.setOnCloseRequest(e->{
+            Platform.runLater(()->{
+                int lastname = getLastId()+1;    
+                try {
+                    File file = new File("map/custom/"+lastname+".png");
+                    file.getParentFile().mkdirs();
+                    ImageIO.write(SwingFXUtils.fromFXImage(map.snapshot(new SnapshotParameters(), null), null), "png", file);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                new MapSerializer("map/custom/"+lastname).export(this.map);
+            });
+        });
+    }
+    private int getLastId() {
+        File folder = new File("map/custom/");
+        if (folder == null || folder.list()==null) {
+            return 1;
+        }
+        return List.of(folder.list()).stream()
+            .filter(filename->filename.endsWith(".map"))
+            .map(filename->Integer.parseInt(filename.replaceAll(".map$", "")))
+            .max(Integer::compareTo).get();
+    }
+    public void drawPane() {
+        pane.setPrefWidth(900);
+        pane.setPrefHeight(627);
+        this.map.setLayoutX(14);
+        this.map.setLayoutY(14);
+        VBox box = new VBox();
+        pane.getChildren().add(box);
+        box.setAlignment(Pos.TOP_CENTER);
+        box.setLayoutX(775);
+        box.setLayoutY(14);
+        box.setPrefHeight(600);
+        box.setPrefWidth(110);
+        box.setStyle("-fx-background-color: black;");
+        for (java.util.Map.Entry<Integer,GameObjectConstructor> entry : CONSTRUCTORS.entrySet()) {
+            Button btn = getButton(entry.getValue().getObject(this.map, 0, 0).getClass().getSimpleName(), e->{
+                GameObject draggable = new Draggable(entry.getValue().getObject(map, 12, 10));
+                draggable.getMask().addEventFilter(MouseEvent.MOUSE_PRESSED,event->{currentFiller = 0;});
+                this.map.addObject(draggable);
+                currentFiller = 0;
+            });
+            box.getChildren().add(btn);
+            VBox.setMargin(btn, new Insets(2, 0, 2, 0));
+        }
+        for (java.util.Map.Entry<Integer,Color> entry : FILLERS.entrySet()) {
+            Button btn = getButton(entry.getValue().toString(), e->{
+                this.currentFiller = entry.getKey();
+            });
+            box.getChildren().add(btn);
+            VBox.setMargin(btn, new Insets(2, 0, 2, 0));
+        }
+    }
+    private Button getButton(String text,EventHandler<ActionEvent> handler) {
+        Button btn = new Button();
+        btn.setText(text);
+        btn.setMnemonicParsing(false);
+        btn.setPrefHeight(20);
+        btn.setPrefWidth(104);
+        btn.getStylesheets().add("/css/main.css");
+        btn.setStyle("-fx-font-size:10px");
+        btn.setOnAction(handler);
+        return btn;
+    }
+}
--- src/main/java/ir/ac/kntu/models/Draggable.java
+++ src/main/java/ir/ac/kntu/models/Draggable.java
@@ -1,11 +1,55 @@
 package ir.ac.kntu.models;
 
 import ir.ac.kntu.map.Map;
+import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
-// public class Draggable extends GameObject {
+public class Draggable extends GameObject {
 
-//     // public Draggable(GameObject go) {
-//     //     super(go.getMap(), go.getGridX(), go.getGridY(), go.getWidth(), go.getHeight(), go.getGridCode(),go.getMask());
-//     // }
-    
-// }
+    private final Delta dragDelta = new Delta();
+
+    public Draggable(GameObject go) {
+        super(go.getMap(), go.getGridX(), go.getGridY(), go.getWidth(), go.getHeight(), go.getGridCode(), go.getMask());
+        getChildren().remove(getMask());
+        // this.setMask(new ImageView());
+        getMap().getChildren().add(getMask());
+        getMask().setLayoutX(getMap().gridToLayout(this.getGridX()));
+        getMask().setLayoutY(getMap().gridToLayout(this.getGridY()));
+        getMask().setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                dragDelta.x = go.getMask().getLayoutX() - mouseEvent.getSceneX();
+                dragDelta.y = go.getMask().getLayoutY() - mouseEvent.getSceneY();
+            }
+        });
+        getMask().setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                // go.setLayoutX();
+                // go.setLayoutY();
+                go.getMask().setLayoutX(mouseEvent.getSceneX() + dragDelta.x);
+                go.getMask().setLayoutY(mouseEvent.getSceneY() + dragDelta.y);
+                // go.getPosition().setX(getMap().layoutToGrid(mouseEvent.getSceneX() + dragDelta.x));
+                // go.getPosition().setY(getMap().layoutToGrid(mouseEvent.getSceneY() + dragDelta.y));
+            }
+        });
+        getMask().setOnMouseReleased(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent arg0) {
+                
+                // System.out.println();
+                getMask().setLayoutX(getMap().gridToLayout(getMap().layoutToGrid(getMask().getLayoutX())));
+                getMask().setLayoutY(getMap().gridToLayout(getMap().layoutToGrid(getMask().getLayoutY())));
+                getPosition().setX(getMap().layoutToGrid(getMask().getLayoutX()));
+                getPosition().setY(getMap().layoutToGrid(getMask().getLayoutY()));
+                // go.getPosition().setY(getMap().layoutToGrid(getMask().getLayoutY()));
+            }
+        });
+    }
+
+}
+
+class Delta {
+    double x, y;
+}
