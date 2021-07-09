f16b690e29a41e236bcab333616f191ed5a3ab0f
mahdi lotfi <74452648+mahdilotfi167@users.noreply.github.com>
2021-07-09 15:17:35 +0430

    fix some pump bugs

--- src/main/java/ir/ac/kntu/models/Pump.java
+++ src/main/java/ir/ac/kntu/models/Pump.java
@@ -17,6 +17,7 @@ public class Pump extends Object {
     private int lenght;
     private int dir;
     private boolean attached;
+    private int counter;
 
     public Pump(Map map) {
         super(map, 0, 0, BLOCK_SCALE, BLOCK_SCALE, new ImageView(), 0);
@@ -26,6 +27,18 @@ public class Pump extends Object {
         circle.setLayoutY(BLOCK_SCALE / 2);
         circle.setVisible(false);
         setRange(3);
+        this.getPosition().addXListener((oldval, newval) -> {
+            if (getMap().isBlock(newval.intValue(), (int) this.getPosition().getY())) {
+                this.active = false;
+                this.circle.setVisible(false);
+            }
+        });
+        this.getPosition().addYListener((oldval, newval) -> {
+            if (getMap().isBlock((int) this.getPosition().getX(), newval.intValue())) {
+                this.active = false;
+                this.circle.setVisible(false);
+            }
+        });
     }
 
     public void setRange(int range) {
@@ -38,38 +51,48 @@ public class Pump extends Object {
             this.getPosition().setX(position.getX());
             this.getPosition().setY(position.getY());
             this.setDirection(direction.getDirection());
+            if (getMap().isBlock(this.getPosition())) {
+                return;
+            }
             this.active = true;
             circle.setVisible(true);
             this.dir = 1;
             attached = false;
+            counter = 0;
         }
     }
 
     @Override
     public void update() {
         if (this.active) {
-            if (lenght == range - 1 && dir > 0) {
-                this.setDirection(getDirection().multiply(-1));
-                dir = -dir;
-            }
-            this.move(this.getDirection());
-            lenght += dir;
-            if (lenght == 0 && dir < 0) {
-                this.active = false;
-                this.circle.setVisible(false);
+            if (counter++ % 3 == 0) {
+                if (lenght == range - 1 && dir > 0) {
+                    this.setDirection(getDirection().multiply(-1));
+                    dir = -dir;
+                }
+                this.move(this.getDirection());
+                lenght += dir;
+                if (lenght == 0 && dir < 0) {
+                    this.active = false;
+                    this.circle.setVisible(false);
+                }
             }
         }
     }
 
     @Override
     public void onCollision(GameObject collider) {
-        if (this.active && !attached) {
-            if (collider instanceof Balloon) {
+        if (this.active) {
+            if (collider instanceof Balloon && !attached) {
                 Balloon b = (Balloon) collider;
                 b.inflate();
                 this.setDirection(getDirection().multiply(-1));
                 dir = -dir;
                 attached = true;
+            }
+            if (collider instanceof Player && dir < 0) {
+                this.active = false;
+                this.circle.setVisible(false);
             }
         }
     }
